9f8537e920ca058a00857ca0a547c244784906ec
AlecksJohannes <alecksjohanssen@gmail.com>
2017-12-30 10:03:09 +0700

    Add libraries

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import 'semantic-ui-css/semantic.min.css';
-import SearchResult from './components/SearchResult';
+import "bulma/css/bulma.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="ui container">
-        <SearchResult />
+      <div className="">
       </div>
     );
   }
