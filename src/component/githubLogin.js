d1d46a75d2a24afcba0512e6716880a28fd81381
AlecksJohannes <alecksjohanssen@gmail.com>
2017-12-31 14:29:27 +0700

    Login with Github is now working

--- src/component/githubLogin.js
+++ src/component/githubLogin.js
@@ -2,55 +2,33 @@ import React, { Component } from 'react';
 import { Button } from 'bloomer';
 import querystring from 'query-string';
 import axios from 'axios';
-const clientID = "4ea83a6419765ad541ac"
-const clientSecret = "0ed6be02055077749f68d1f55b4e0cc37739a646"
 
 class GitHubLogin extends Component {
-
-	clientSecret = "0ed6be02055077749f68d1f55b4e0cc37739a646"
 	githubLogin = () => {
-		// if (!window.sessionStorage.getItem('key'))
-		// {
-		this.fetchDataGithub()
-		// }
-		// else
-		// {
-		// 	alert(" you are login")
-		// }
+    this.fetchDataGithub(window.open(`http://localhost:3000/auth/github`, "cODERSCHOOL", "toolbar=no, scrollbars=no, resizable=no, height=200, width=200"))
 	}
 
-	fetchDataGithub() {
-		var a = window.open(`https://github.com/login/oauth/authorize?scope=user:email&client_id=${clientID}`, "cODERSCHOOL", "height=600, width=1000") 
-			var url_code = querystring.parse(a.location.search)
-			setInterval( () => {
-				a.window.close();
-			},2000
-			)
-			if (a.window.closed) {
-				var url_code = querystring.parse(a.location.search)
-			console.log(url_code)
-			if (url_code.code) {
-			axios.post("https://github.com/login/oauth/access_token", {
-						webSecurity: false,
-						client_id: clientID,
-						client_secret: clientSecret,
-						code: url_code.code,
-						headers: {
-							'crossDomain': true
-						}
-				})
-				.then(function (res) { return res.json(); })
-				.then(function (data) { alert(JSON.stringify(data)) })
-		}
-	}
+  fetchDataGithub(popup) {
+    if(popup.closed) {
+    } else {
+      try {
+        var url_code = querystring.parse(popup.location.search)
+      } catch(e) {}
+      if (url_code.access_token) {
+        window.sessionStorage.setItem('key', JSON.stringify(url_code))
+        popup.close()
+      } else {
+        setTimeout(this.fetchDataGithub.bind(this, popup), 0)
+      }
+    }
+  }
+
 	render() {
-		return (
-
-			<Button isColor='dark' style={{ width: 100 }} onClick={() => this.githubLogin()}>
-				Github
-				</Button>
-
-		)
+    return (
+      <Button isColor='dark' style={{ width: 100 }} onClick={() => this.githubLogin()}>
+        Github
+      </Button>
+    )
 	}
 }
 
